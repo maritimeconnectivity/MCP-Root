dc6156fcaad1c09b83a8519fb51510c6f43876dc
Oliver Haagh <oliver@dmc.international>
2020-09-10 16:42:27 +0200

    Can now get and create root ca's

--- src/main/java/net/maritimeconnectivity/rootcalist/controllers/RootCAController.java
+++ src/main/java/net/maritimeconnectivity/rootcalist/controllers/RootCAController.java
@@ -16,19 +16,32 @@
 
 package net.maritimeconnectivity.rootcalist.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import net.maritimeconnectivity.rootcalist.model.RootCA;
 import net.maritimeconnectivity.rootcalist.services.RootCAService;
+import org.bouncycastle.cert.CertException;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509ContentVerifierProviderBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.operator.OperatorCreationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Date;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/")
 public class RootCAController {
@@ -50,5 +63,51 @@ public class RootCAController {
             return new ResponseEntity<>(this.rootCAService.listByAttestors(attestorIds), HttpStatus.OK);
         }
         return new ResponseEntity<>(this.rootCAService.listAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/root/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<RootCA> getRootCA(@PathVariable Long id) {
+        RootCA rootCA = this.rootCAService.getById(id);
+        if (rootCA != null) {
+            return new ResponseEntity<>(rootCA, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(
+            value = "/root",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<RootCA> createRootCA(@RequestBody RootCA rootCA) {
+        PEMParser pemParser = new PEMParser(new StringReader(rootCA.getCertificate()));
+        try {
+            X509CertificateHolder certificateHolder = (X509CertificateHolder) pemParser.readObject();
+            if (certificateHolder.isValidOn(new Date()) && isSelfSigned(certificateHolder)) {
+                RootCA newRootCA = this.rootCAService.save(rootCA);
+                return new ResponseEntity<>(newRootCA, HttpStatus.OK);
+            }
+        } catch (IOException e) {
+            log.error("New root CA certificate could not be read");
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    private boolean isSelfSigned(X509CertificateHolder certificateHolder) {
+        if (certificateHolder.getSubject().equals(certificateHolder.getIssuer())) {
+            JcaX509ContentVerifierProviderBuilder contentVerifierProviderBuilder = new JcaX509ContentVerifierProviderBuilder();
+            contentVerifierProviderBuilder.setProvider(new BouncyCastleProvider());
+            try {
+                return certificateHolder.isSignatureValid(contentVerifierProviderBuilder.build(certificateHolder));
+            } catch (CertException | OperatorCreationException e) {
+                return false;
+            }
+        }
+        return false;
     }
 }
